c085c7cec71f2dbf02f705b013b497bfaeb8e92b
seokmin <msm0748@gmail.com>
2024-11-08 15:24:20 +0900

    feat: sass 설정 및 style 추가

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,26 @@
 import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { sveltePreprocess } from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: sveltePreprocess({
+		scss: {
+			prependData: "@import 'src/styles/_variables.scss'; @import 'src/styles/_utilities.scss';"
+		}
+	}),
 
 	kit: {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		alias: {
+			$styles: 'src/styles',
+			$stores: 'src/stores',
+			$types: 'src/types'
+		}
 	}
 };
 
